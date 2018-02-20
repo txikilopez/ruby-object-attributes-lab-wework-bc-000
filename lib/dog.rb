1e85b9c9e46352eb9f1caef6efe95c131229a11e
Antonio Troncoso <antonio.troncoso@wework.com>
2018-02-20 00:57:23 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+  def breed=(type)
+    @breed = type
+  end
+  
+  def breed
+    @breed
+  end
+  
+  def name=(get_name)
+    @name = get_name
+  end
+  def name
+    @name
+  end
+  
+  
+end
---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name=(type)
+    @name = type
+  end
+  
+  def name
+    @name
+  end
+  
+  def job=(get_job)
+    @job = get_job
+  end
+
+  def job
+    @job
+  end
+end
